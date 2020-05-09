8258c2c612980ca7293f103c8eb57d80beba1d06
root <root@ip-10-0-38-216.ap-southeast-1.compute.internal>
2020-05-09 15:23:42 +0000

    Developer B Changes

--- src/main/java/com/ms/calc/CalculateServlet.java
+++ src/main/java/com/ms/calc/CalculateServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//Devloper B Changes
 @WebServlet(name = "CalculateServlet", urlPatterns = {"/CalculateServlet"}, initParams={
 @WebInitParam(name="no1", value=""), 
 @WebInitParam(name="no2", value=""),
